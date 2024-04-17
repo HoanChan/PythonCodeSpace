78dfe3d700f153a9608fa193fe3ab1a7968a130c
Lê Hoàn Chân <Le.Hoan.Chan@gmail.com>
2024-04-17 02:25:21 +0000

    Bổ sung thư viện pytest

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Đây là template cho việc tạo một github repo với codespace đủ để viết code trực tuyến trên Codespace.
 
-Code chính sẽ nằm trong thư mục `src` và các test nằm trong thư mục `tests`
+Code chính sẽ nằm trong thư mục `src` và các test nằm trong thư mục `tests`. Việc test sẽ được thực hiện dựa vào thư viện `pytest`
 
 Đã thiết lập cài đặt sẵn các tiện ích mở rộng gồm:
 
--- tests/test_plus.py
+++ tests/test_plus.py
@@ -1,5 +1,16 @@
 import pytest
 
-def test_plus():
-    assert 1 + 1 == 2
+class TestPlus:
+    @pytest.mark.parametrize("a, b, expected", [
+        (1, 1, 2),
+        (2, 2, 4),
+        (10, 10, 20),
+        (0, 0, 0),
+        (0, 1, 1),
+        (0, -1, -1),
+        (-1, -1, -2),
+        (-1, 1, 0),
+    ])
+    def test_plus(self, a, b, expected):
+        assert 1 + 1 == 2
 
